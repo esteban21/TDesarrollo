101379dcfe61dbcc27ae012d5a45f1648bdc0078
nbascuna <nicol.b1101@gmail.com>
2017-07-14 16:06:41 -0400

    Interfaz

--- views/site/index.php
+++ views/site/index.php
@@ -7,46 +7,108 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Universidad de Macondo</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Bienvenido a nuestro servicio de Convenios.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://localhost/TDesarrollo/web/index.php?r=site%2Flogin">Ingresar</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
+
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Actividad Convenio</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-actividad-convenio/index">Ir Actividad Convenio &raquo;</a></p>
             </div>
+
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Estado Actividad</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-estado-actividad/index">Ir Estado Actividad &raquo;</a></p>
             </div>
+
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Tipo Actividad</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-tipo-actividad/index">Ir Tipo Actividad &raquo;</a></p>
             </div>
+
+            <div class="col-lg-4">
+                <h2>Responsable Actividad</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-responsable-actividad/index">Ir Responsable Actividad &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Coordinador Convenio</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-coordinador-convenio/index">Ir Coordinador Convenio &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Modalidad Colaboracion</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-modalidad-colaboracion/index">Ir Modalidad Colaboracion &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Area Convenio</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-area-convenio/index">Ir Area Convenio &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Convenio</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-convenio/index">Ir Convenio &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Institucion</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-insitucion/index">Ir Institucion &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Objetivo Convenio</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-objetivo-convenio/index">Ir Objetivo Convenio &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Pais</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-pais/index">Ir Pais &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Tipo Convenio</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-tipo-convenio/index">Ir Tipo Convenio &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Tipo Institucion</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-tipo-institucion/index">Ir Tipo Institucion &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Contenidos en</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-contenidos-en/index">Ir Contenidos en &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Coordinados por</h2>
+
+                <p><a class="btn btn-default" href="http://localhost/TDesarrollo/web/index.php?r=cnv-coordinados-por/index">Ir Coordinados por &raquo;</a></p>
+            </div>
+
+
         </div>
 
     </div>
